b05a5e726451bb48214a968f3834eb6c8cb5c5e8
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-16 19:54:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+let list=document.getElementById("infi-list");
+let i;
+for( i=1;i<=10;i++){
+    list.innerHTML+=`<p>Item ${i}</p>`
+}
+i--;
+function handleScroll(){
+    i++;
 
+    list.innerHTML+=`<p>Item ${i}</p>`
+    i++;
+
+    list.innerHTML+=`<p>Item ${i}</p>`
+}
+list.addEventListener("scroll",()=>{
+    if(list.clientHeight>=list.scrollHeight-list.scrollTop){
+        handleScroll();
+    }
+})
